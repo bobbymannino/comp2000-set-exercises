1af25130b586fd144f4cfc11f8c7c3da6dad07b8
Bobby Mannino <manninobobby@icloud.com>
2024-10-11 13:21:42 +0100

    added on click for login button
    33% of error, admin side or employee side

--- app/src/main/java/com/example/comp2000setexercises/MainActivity.java
+++ app/src/main/java/com/example/comp2000setexercises/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.comp2000setexercises;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginButton;
+    TextView errorLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loginButton = findViewById(R.id.loginButton);
+        errorLabel = findViewById(R.id.errorLabel);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double choice = Math.random() * 3;
+
+                // If i show the error on the prior click, it must be hidden on the next attempt
+                hideError();
+
+                if (choice < 1) {
+                   // Goto emplyee side
+                } else if (choice < 2) {
+                    // Goto admin side
+                } else {
+                    showError();
+                }
+
+            }
+        });
+    }
+
+    void hideError() {
+        errorLabel.setVisibility(View.GONE);
+    }
+
+    void showError() {
+        errorLabel.setVisibility(View.VISIBLE);
     }
 }
