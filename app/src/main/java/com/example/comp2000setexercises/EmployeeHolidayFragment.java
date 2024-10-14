0bb839a92650bccb2ac91b9c28bc3509990130f5
Bobby Mannino <manninobobby@icloud.com>
2024-10-14 20:43:12 +0100

    working on adding new holiday request page

--- app/src/main/java/com/example/comp2000setexercises/EmployeeHolidayFragment.java
+++ app/src/main/java/com/example/comp2000setexercises/EmployeeHolidayFragment.java
@@ -1,5 +1,6 @@
 package com.example.comp2000setexercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EmployeeHolidayFragment extends Fragment {
+    FloatingActionButton newHolidayFab;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employee_holiday, container, false);
+        View view = inflater.inflate(R.layout.fragment_employee_holiday, container, false);
+
+        newHolidayFab = view.findViewById(R.id.newHolidayFab);
+
+        newHolidayFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewHolidayPopover();
+            }
+        });
+
+        return view;
+    }
+
+    void openNewHolidayPopover() {
+        // TODO sort this out
     }
 }
---
+++ app/src/main/java/com/example/comp2000setexercises/EmployeeNewHolidayFragment.java
@@ -0,0 +1,24 @@
+package com.example.comp2000setexercises;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EmployeeNewHolidayFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_employee_new_holiday, container, false);
+
+        return view;
+    }
+}
