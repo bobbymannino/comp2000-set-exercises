1185d0b0e96cdc8aac6eae22fbacfb24b561e492
Bobby Mannino <manninobobby@icloud.com>
2024-10-14 19:49:19 +0100

    added button click on employee settings
    click doesnt do anything but is ready for logic

--- app/src/main/java/com/example/comp2000setexercises/EmployeeSettingsFragment.java
+++ app/src/main/java/com/example/comp2000setexercises/EmployeeSettingsFragment.java
@@ -1,14 +1,19 @@
 package com.example.comp2000setexercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class EmployeeSettingsFragment extends Fragment {
+    Button goHomeButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +22,16 @@ public class EmployeeSettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employee_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_employee_settings, container, false);
+        goHomeButton = view.findViewById(R.id.goHomeButton);
+
+        goHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO call goHome in EmployeeActivity.java
+            }
+        });
+
+        return view;
     }
 }
