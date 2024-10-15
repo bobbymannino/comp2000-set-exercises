bd3eb9e60e85fff5b649fc4fb22177c8f1c39a75
Bobby Mannino <manninobobby@icloud.com>
2024-10-15 11:54:13 +0100

    added holiday request sheet

--- app/src/main/java/com/example/comp2000setexercises/EmployeeHolidayFragment.java
+++ app/src/main/java/com/example/comp2000setexercises/EmployeeHolidayFragment.java
@@ -1,8 +1,10 @@
 package com.example.comp2000setexercises;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EmployeeHolidayFragment extends Fragment {
@@ -33,6 +36,19 @@ public class EmployeeHolidayFragment extends Fragment {
     }
 
     void openNewHolidayPopover() {
-        // TODO sort this out
+        Context ctx = getContext();
+        BottomSheetDialog sheet = new BottomSheetDialog(ctx);
+        View view = LayoutInflater.from(ctx).inflate(R.layout.fragment_employee_new_holiday, null);
+        sheet.setContentView(view);
+
+        Button btn = view.findViewById(R.id.submitRequestButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sheet.hide();
+            }
+        });
+
+        sheet.show();
     }
 }
